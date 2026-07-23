57632e9ef2297025984eb1025db5794f7bca5468
akul12653 <bansalakul123@gmail.com>
2026-07-23 11:45:18 -0700

    Added plan

--- Circuit.py
+++ Circuit.py
@@ -1,6 +1,21 @@
 import cirq
 
-c = cirq.Circuit()
-q0 = cirq.NamedQubit("qubit")
-c.append(cirq.H(q0))
-print(c)
+# function 1: def exp_value(n_00, n_11, n_01, n_10) -> calculates expected value or E(x, y)
+# function 2: def chsh_value(x, y, z, d) --> calculates CHSH value
+
+#first entangled circuit with A, B
+#second entangled circuit with A, B'
+#third entangled circuit with A', B
+#fourth entangled circuit with A', B'
+
+#run simulations on the circuit 
+#create histogram on each qubit value
+#n_11 = count_through the histogram*(+1)
+#n_01 = count_through the histogram*(-1)
+#n_00 = count_through the histogram*(+1)
+#n_10 = count_through the histogram*(-1)
+#call exp_value function apply to each circuit
+#do lines 11-17 4 times for each circuit
+
+#store the exp_values for each circuit
+#call chsh function using each exp_value
