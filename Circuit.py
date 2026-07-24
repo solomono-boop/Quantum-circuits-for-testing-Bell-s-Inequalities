d47e1895848c6b7c8afece2ac278b826b0fe8197
zshonazych <zshonazych@gmail.com>
2026-07-24 15:05:30 -0400

    Added CHSH calculations,correlation calculations, and our plots

--- Circuit.py
+++ Circuit.py
@@ -1,5 +1,6 @@
 import cirq
 import matplotlib.pyplot as plt
+import numpy as np
 
 # function 1: def exp_value(n_00, n_11, n_01, n_10) -> calculates expected value or E(x, y)
 #n_11= number of times Alice measured 1 and Bob measured 1
@@ -32,20 +33,34 @@ def chsh_value(x,y,z,d):
 def sim_run(circuit, reps):
     simulator = cirq.Simulator()
     result = simulator.run(circuit, repetitions=reps)
-    count = result.histogram(key="result")
-    #next line creates histogram, commented out for now
-    #hist = cirq.plot_state_histogram(result, plt.subplot(), title = 'Qubit States', xlabel = 'States', ylabel = 'Occurrences')
-    plt.show()
-    return count
+    counts = result.histogram(key="result")
+    #Creating the histogram: COMMENTED OUT
+    #We wanted it to be obvious how many occurances happen per each state hence why we did it this way
+
+    #Convert Cirq's interger states into binary numbers so its easier to see the combinations 
+    #states=["00","01","10","11"]
+    #occurances=[counts.get(0,0),counts.get(1,0),counts.get(2,0),counts.get(3,0),]
+
+    #creating histogram:
+   #plt.bar(states,occurances,color="#ff69b4")
+    #plt.xlabel("Measurement State (Alice,Bob)")
+    #plt.ylabel("Occurances")
+    #plt.title("Qubit Measurement Results")
+    #plt.show()
+    return counts
 
 #def get_exp_value(count_of_each_value) 
 #n_11 = count..*(+1)
 #n_01 = count..*(-1)
 #n_00 = count..*(+1)
 #n_10 = count..*(-1)
-#call exp_value function apply to the circuit
-#store the exp_values for the circuit
-#return value should be exp_value of the circuit
+def get_exp_value(counts):
+    n_00=counts.get(0,0)
+    n_01=counts.get(1,0)
+    n_10=counts.get(2,0)
+    n_11=counts.get(3,0)
+    return exp_value(n_00,n_11,n_01,n_10)
+
 
 #first entangled circuit with A, B
 a = cirq.NamedQubit("q0")
@@ -53,7 +68,7 @@ b = cirq.NamedQubit("q1")
 circuit1 = cirq.Circuit()
 circuit1.append(cirq.H(a))
 circuit1.append(cirq.CNOT(a, b))
-circuit1.append((cirq.Y ** 0.25)(b))
+circuit1.append((cirq.ry(np.pi/4)(b)))
 circuit1.append(cirq.measure(a,b, key="result"))
 
 #second entangled circuit with A, B'
@@ -62,7 +77,7 @@ b_dash = cirq.NamedQubit("q1")
 circuit2 = cirq.Circuit()
 circuit2.append(cirq.H(a))
 circuit2.append(cirq.CNOT(a, b_dash))
-circuit2.append((cirq.Y ** (-0.25))(b_dash))
+circuit2.append((cirq.ry(-(np.pi/4)))(b_dash))
 circuit2.append(cirq.measure(a, b_dash, key="result"))
 
 #third entangled circuit with A', B
@@ -71,7 +86,8 @@ b = cirq.NamedQubit("q1")
 circuit3 = cirq.Circuit()
 circuit3.append(cirq.H(a_dash))
 circuit3.append(cirq.CNOT(a_dash, b))
-circuit3.append((cirq.Y ** (0.5))(a_dash))
+circuit3.append(cirq.ry(np.pi/2)(a_dash))
+circuit3.append(cirq.ry(np.pi/4)(b))
 circuit3.append(cirq.measure(a_dash, b, key="result"))
 
 #fourth entangled circuit with A', B'
@@ -80,9 +96,43 @@ b_dash = cirq.NamedQubit("q1")
 circuit4 = cirq.Circuit()
 circuit4.append(cirq.H(a_dash))
 circuit4.append(cirq.CNOT(a_dash, b_dash))
-circuit4.append((cirq.Y ** (0.5))(a_dash))
-circuit4.append((cirq.Y ** (-0.25))(b_dash))
+circuit4.append((cirq.ry(np.pi/2))(a_dash))
+circuit4.append((cirq.ry(-(np.pi/4)))(b_dash))
 circuit4.append(cirq.measure(a_dash, b_dash, key="result"))
 
+#testing how CHSH values changes with different numbers of repeitions
+rep_values=[50,100,500,1000,2500,5000]
+#stores CHSH value for each repitiion amount
+chsh_results=[]
+#run expirement for each repition amount
+for reps in rep_values:
+    counts1=sim_run(circuit1,reps)
+    counts2=sim_run(circuit2,reps)
+    counts3=sim_run(circuit3,reps)
+    counts4=sim_run(circuit4,reps)
+    #store the exp_values for the circuit
+    E_AB= get_exp_value(counts1)
+    E_AB_prime= get_exp_value(counts2)
+    E_A_prime_B= get_exp_value(counts3)
+    E_A_prime_B_prime= get_exp_value(counts4)
+    #call chsh function using each exp_value
+    S=chsh_value(E_AB,E_AB_prime,E_A_prime_B,E_A_prime_B_prime)
+    #saving result
+    chsh_results.append(S)
+    print("Repetitions: ",reps)
+    print("E(A,B):", E_AB)
+    print("E(A,B'):", E_AB_prime)
+    print("E(A',B):", E_A_prime_B)
+    print("E(A',B'):", E_A_prime_B_prime)
+    print("CHSH value: ",S)
+    print("---------------")
 
-#call chsh function using each exp_value
+#Create CHSH graph
+plt.plot(rep_values,chsh_results,marker="*",color="#ff69b4",label="Simulated CHSH value")
+plt.axhline(y=2,color="#3D3D3D",linestyle="--",label="Classical Limit of 2")
+plt.axhline(y=2*np.sqrt(2),color="#898989",linestyle="--",label="Tsirelson's bound (2√2)")
+plt.title("CHSH Value vs Number of Measurement")
+plt.xlabel("Number of repeititions")
+plt.ylabel("CHSH value")
+plt.legend()
+plt.show()
